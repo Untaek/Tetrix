fbb81cc4e00e9a09afbf32ce4dcd03c39eec5585
sbwoo96 <sbwoo96@hanmail.net>
2018-10-24 00:53:07 +0900

    modify the code

--- src/untaek/StartScreen.java
+++ src/untaek/StartScreen.java
@@ -1,37 +1,34 @@
 package untaek;
 
+import untaek.Client;
 import javax.swing.*;
 import javax.swing.border.LineBorder;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class StartScreen extends JPanel {
 
     public StartScreen() {
 
         JPanel ss = new JPanel();
+        JPanel lf = new JPanel();
+        JLabel id = new JLabel("ID");
+        JTextField tf = new JTextField(10);
+        JLabel pw = new JLabel("PW");
+        JPasswordField pf = new JPasswordField(10);
+        JButton join = new JButton("Join");
+
         ss.setLayout(new BorderLayout());
         ss.setBorder(new LineBorder(Color.BLACK, 1, true));
         ss.setPreferredSize(new Dimension(500, 700));
+
         add(ss, BorderLayout.CENTER);
-
-        JPanel lf = new JPanel();
-        ss.add(lf, BorderLayout.NORTH);
-
-        JLabel id = new JLabel("ID");
+        ss.add(lf);
         lf.add(id);
-
-        JTextField tf = new JTextField(10);
         lf.add(tf);
-
-        JLabel pw = new JLabel("PW");
         lf.add(pw);
-
-        JPasswordField pf = new JPasswordField(10);
         lf.add(pf);
-
-        JButton join = new JButton("Join");
-
-        ss.add(join);
-
+        lf.add(join);
     }
 }
